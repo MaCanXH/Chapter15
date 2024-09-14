6ba2b10f84b9c3d4e48a95b1ef2e7acfbedfb376
Max Chan <maxchan@gmail.com>
2024-09-14 12:15:15 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+string path = @"exercise.txt";
+
+StreamWriter writer = new StreamWriter(path, true);
+
+using (writer)
+{
+    writer.WriteLine("中文嘗試");
+}
+
+StreamReader reader = new StreamReader(path);
+
+using (reader)
+{
+    int linenumber = 0;
+
+    string line = reader.ReadLine();
+
+    while(line != null)
+    {
+        linenumber++;
+        Console.WriteLine(line);
+        line = reader.ReadLine();
+    }
+
+}
+
